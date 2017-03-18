6a0ebd3f99c6c637c2e2fba496321bd641ccb6e2
Sam Jackson <smahstan@gmail.com>
2017-03-18 13:32:05 -0500

    Created "index.js" renderer process for "index.html"

---
+++ script/index.js
@@ -0,0 +1,6 @@
+//Renderer process for main window
+console.log("Node.js version: ", process.versions.node)
+console.log("Chromium version: ", process.versions.chrome)
+console.log("Electron version: ", process.versions.electron)
+
+document.write("<p>Testing to see if render process working.</p>")
