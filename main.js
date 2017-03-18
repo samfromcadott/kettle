13ce37631f3b5dd98f2863be9fd8e4d02b4d487a
Sam Jackson <smahstan@gmail.com>
2017-03-18 11:57:17 -0500

    Window starts maximized

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let mainWindow
 function createWindow () {
 	// Create the browser window.
 	mainWindow = new BrowserWindow({width: 800, height: 600})
+	mainWindow.maximize()
 
 	// and load the index.html of the app.
 	mainWindow.loadURL(url.format({
@@ -21,9 +22,6 @@ function createWindow () {
 		protocol: 'file:',
 		slashes: true
 	}))
-
-	// Open the DevTools.
-	mainWindow.webContents.openDevTools()
 
 	// Emitted when the window is closed.
 	mainWindow.on('closed', function () {
