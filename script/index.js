a9a06ad5da92bc1b9a522a09f92cd2a2a50b21ef
Sam Jackson <smahstan@gmail.com>
2017-03-18 13:37:52 -0500

    Now using JQuery.

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 //Renderer process for main window
+var $ = require('jQuery')
+
 console.log("Node.js version: ", process.versions.node)
 console.log("Chromium version: ", process.versions.chrome)
 console.log("Electron version: ", process.versions.electron)
 
-document.write("<p>Testing to see if render process working.</p>")
+$("body").append("<p>Testing to see if render process working.</p>")
