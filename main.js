3aac0a1ec30948a180b1b86982f95ff8a2a4cc58
Sam Jackson <smahstan@gmail.com>
2017-03-19 15:16:49 -0500

    Using Tone.js now

--- main.js
+++ main.js
@@ -9,28 +9,33 @@ const {ipcMain} = require('electron')
 const path = require('path')
 const url = require('url')
 
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+var windowArray = []
+exports.windowCount = 0
 
 function createWindow () {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600})
-	mainWindow.maximize()
+	// Create the new browser window.
+	windowArray.push( new BrowserWindow({width: 800, height: 600}) )
+	windowCount = windowArray.length
+	newWindow = windowArray[windowCount-1]
+
+	// windowArray[windowCount-1].maximize()
 
 	// and load the index.html of the app.
-	mainWindow.loadURL(url.format({
+	newWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'html/index.html'),
 		protocol: 'file:',
 		slashes: true
 	}))
 
 	// Emitted when the window is closed.
-	mainWindow.on('closed', function () {
+	newWindow.on('closed', function () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null
+		newWindow = null
 	})
 }
 
@@ -48,20 +53,25 @@ app.on('window-all-closed', function () {
 	}
 })
 
-app.on('activate', function () {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow()
-	}
-})
+// app.on('activate', function () {
+// 	// On OS X it's common to re-create a window in the app when the
+// 	// dock icon is clicked and there are no other windows open.
+// 	if (mainWindow === null) {
+// 		createWindow()
+// 	}
+// })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-exports.testString = "Requiring main process corectly";
+exports.testString = "Requiring main process corectly"
 
 exports.domainTest = function () {
 	//Testing to see where console.log ends up.
-	console.log("domainTest() was run.");
-};
+	console.log("domainTest() was run")
+}
+
+ipcMain.on('window-manager', (event, arg) => {
+	console.log(arg)
+
+	if (arg == "New Window") { //Create new window
+		createWindow()
+	}
+})
--- script/index.js
+++ script/index.js
@@ -5,12 +5,18 @@ const {remote} = electron
 const {Menu}  = remote.require('electron')
 const {ipcRenderer} = require('electron')
 const main = remote.require('./main')
+const Tone = require('Tone')
+
+// //Check to see if this is the first window
+// if (main.windowCount == 0) { //windowCount isn't increased until after creation
+// 	console.log(main.windowCount)
+// 	const Tone = require('Tone') //Only one instance of Tone should be created
+// }
 
 console.log("Node.js version: ", process.versions.node)
 console.log("Chromium version: ", process.versions.chrome)
 console.log("Electron version: ", process.versions.electron)
 
-console.log("Testing to see if render process working.")
-console.log(main.testString)
-
-main.domainTest()
+// $('#new-window').click(function() {
+// 	ipcRenderer.send('window-manager', 'New Window')
+// })
