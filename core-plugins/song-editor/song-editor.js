819814be38c3ceded5b24193781a3aef29a640ba
Sam Jackson <smahstan@gmail.com>
2017-05-08 08:51:36 -0500

    Song editor now has start of track system

--- core-plugins/song-editor/song-editor.js
+++ core-plugins/song-editor/song-editor.js
@@ -1,4 +1,22 @@
-console.log('Works')
+$('#play').click( () => {
+	audio.play()
+})
+
+$('#stop').click( () => {
+	audio.stop()
+})
+
+$('#pause').click( () => {
+	audio.pause()
+})
+$( "#metronome-checkbox" ).change(function () {
+	if (this.checked) {
+		audio.metronome.changeMode('on')
+	}
+	else {
+		audio.metronome.changeMode('off')
+	}
+})
 
 $('#volume-control').on('input', function () {
 	audioTrack.volume.set( 'volume', Math.log( $(this).val() )*36 )
@@ -16,3 +34,7 @@ $('#update-program').click( function () {
 	clip.length = $('#length').val() + 'm'
 	audioTrack.part.add( clip )
 })
+
+// $('#new-track').click( function () {
+//
+// })
